953e479d3f67c6f7c8959223b3fadb6548326003
Dan Kirkwood <daniel.g.kirkwood@gmail.com>
2022-09-07 11:29:26 +1000

    fix VPC naming

--- teleport_targets/kubernetes/eks/vpc.tf
+++ teleport_targets/kubernetes/eks/vpc.tf
@@ -5,7 +5,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "dk-eks-${random_string.suffix.result}"
+  cluster_name = var.clustername
 }
 
 resource "random_string" "suffix" {
@@ -17,7 +17,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.2.0"
 
-  name                 = "dk-k8s-vpc"
+  name                 = "${local.cluster_name}-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
