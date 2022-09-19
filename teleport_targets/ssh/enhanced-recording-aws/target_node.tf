001180f2e63091052a28fd145c90c6ee19aea21b
Dan Kirkwood <daniel.g.kirkwood@gmail.com>
2022-09-19 17:23:47 +1000

    Add enhanced recording directory

---
+++ teleport_targets/ssh/enhanced-recording-aws/target_node.tf
@@ -0,0 +1,40 @@
+data "aws_ami" "target" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = [var.ami_name]
+  }
+  owners = [var.ami_owner]
+}
+
+
+
+resource "aws_instance" "target_nodes" {
+  for_each = var.machines
+  ami           = data.aws_ami.target.id
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.server.id
+  vpc_security_group_ids = [ aws_security_group.serveraccess.id ]
+  key_name = var.key_name
+  user_data = <<EOF
+#!/bin/bash
+echo "Changing hostname..."
+sudo hostnamectl set-hostname ${each.value.hostname}
+sudo tee -a /etc/teleport.yaml <<'EOT'
+
+  enhanced_recording:
+    enabled: true
+EOT
+sudo systemctl restart teleport
+EOF
+  metadata_options {
+    http_endpoint = "enabled"
+    instance_metadata_tags = "enabled"
+  }
+
+  tags = {
+    Name = "${each.value.hostname}"
+    Env = "${each.value.environment}"
+  }
+}
