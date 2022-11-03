740a3b07461f9a826f97ec18a7ae8513ccedcdb1
GERey <GERey@users.noreply.github.com>
2022-11-03 10:27:10 -0700

    Removing reference to hardcoded key to use var

--- teleport_cluster/single_node/aws/proxy_node.tf
+++ teleport_cluster/single_node/aws/proxy_node.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "proxy_node" {
   associate_public_ip_address = true
   vpc_security_group_ids = [ aws_security_group.serveraccess.id ]
   iam_instance_profile = aws_iam_instance_profile.ec2_describe.name
-  key_name = "dk-rsa"
+  key_name = var.key
   metadata_options {
     http_endpoint = "enabled"
     instance_metadata_tags = "enabled"
