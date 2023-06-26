82ff4a06070090c777ae9852a0762052483b4950
Dan Kirkwood <daniel.g.kirkwood@gmail.com>
2023-06-26 11:22:58 +1000

    bump auth package version

--- teleport_targets/windows_desktop/aws-non-ad/packer/teleport-install.ps1
+++ teleport_targets/windows_desktop/aws-non-ad/packer/teleport-install.ps1
@@ -1,3 +1,3 @@
-Invoke-WebRequest -Uri https://cdn.teleport.dev/teleport-windows-auth-setup-v13.0.0-amd64.exe -OutFile C:\Users\Administrator\Downloads\teleport_windows.exe
+Invoke-WebRequest -Uri https://cdn.teleport.dev/teleport-windows-auth-setup-v13.1.1-amd64.exe -OutFile C:\Users\Administrator\Downloads\teleport_windows.exe
 cd "C:\Users\Administrator\Downloads"
 .\teleport_windows.exe install --cert=C:\Users\Administrator\Downloads\teleport.cer
