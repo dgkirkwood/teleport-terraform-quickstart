03ade3c9b16bd3b046541aeab19927a65c700bb5
Dan Kirkwood <daniel.g.kirkwood@gmail.com>
2023-08-16 15:51:03 +1000

    add kubeconfig command

--- teleport_cluster/azure_aks_ha/outputs.tf
+++ teleport_cluster/azure_aks_ha/outputs.tf
@@ -1,3 +1,7 @@
 output "cluster-url" {
   value = "https://${azurerm_dns_a_record.teleport.fqdn}"
 }
+
+output "kubeconfig_command" {
+  value = "az aks get-credentials --resource-group ${azurerm_resource_group.teleport.name} --name ${azurerm_kubernetes_cluster.teleport.name}"
+}
