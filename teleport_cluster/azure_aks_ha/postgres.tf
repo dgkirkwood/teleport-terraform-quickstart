26d9f024437777ed98f9cdd49aaf42721d803a5d
Dan Kirkwood <daniel.g.kirkwood@gmail.com>
2023-08-16 12:56:40 +1000

    add databases to Teleport state for easier deletion

--- teleport_cluster/azure_aks_ha/postgres.tf
+++ teleport_cluster/azure_aks_ha/postgres.tf
@@ -31,13 +31,12 @@ resource "azurerm_postgresql_flexible_server_configuration" "wal" {
 resource "azurerm_postgresql_flexible_server_firewall_rule" "access" {
   name = "AllowK8s"
   server_id = azurerm_postgresql_flexible_server.teleport.id
-  start_ip_address = "192.168.1.1"
-  end_ip_address = "192.168.1.254"
+  start_ip_address = "0.0.0.0"
+  end_ip_address = "255.255.255.255"
 }
 
 # Assign the Teleport Service Principal as an admin on the PostgreSQL Flexible Server
 resource "azurerm_postgresql_flexible_server_active_directory_administrator" "admin" {
-  depends_on = [ azurerm_postgresql_flexible_server_configuration.wal ]
   server_name = azurerm_postgresql_flexible_server.teleport.name
   resource_group_name = azurerm_resource_group.teleport.name
   tenant_id = data.azurerm_client_config.current.tenant_id
@@ -45,4 +44,18 @@ resource "azurerm_postgresql_flexible_server_active_directory_administrator" "ad
   principal_name = azurerm_user_assigned_identity.teleport.name
   object_id = azurerm_user_assigned_identity.teleport.principal_id
 
+}
+
+resource "azurerm_postgresql_flexible_server_database" "teleport_backend" {
+  server_id = azurerm_postgresql_flexible_server.teleport.id
+  name = "teleport_backend"
+  collation = "C"
+  charset = "UTF8"
+}
+
+resource "azurerm_postgresql_flexible_server_database" "teleport_audit" {
+  server_id = azurerm_postgresql_flexible_server.teleport.id
+  name = "teleport_audit"
+  collation = "C"
+  charset = "UTF8"
 }
